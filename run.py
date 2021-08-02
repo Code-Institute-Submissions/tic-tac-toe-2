636b4253ffa016643d2ff2a76eab64f980f96f6c
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2021-08-02 18:16:45 +0000

    add gamearea / board and function for updating gamearea / board after everymove.

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-# python code goes here
+# Game area / board
+
+gamearea = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+
+gamearea_keys = []
+ 
+for key in gamearea:
+    gamearea_keys.append(key)
+    
+''' 
+prints the updated board after every move.
+ '''
+ 
